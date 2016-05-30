53559c1807ddfc447f705944ca1b5c0a9cec30d4
piccibhai <picci.bhai@hotmail.com>
2016-05-30 21:35:29 +0600

    Testing

--- Test/Controllers/HomeController.cs
+++ Test/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 using Test.DAL;
 using Test.Models;
 
@@ -10,17 +11,27 @@ namespace Test.Controllers
 {
     public class HomeController : Controller
     {
-        private TestDbContext cxt = new TestDbContext();
+        private TestDbContext cxt;
+
+        public HomeController()
+        {
+            cxt=new TestDbContext();
+        }
 
         public ActionResult Index()
         {
             
-            TestDb tdb = new TestDb();
-            tdb.name = "piccibhai";
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(TestDb tdb)
+        {
             cxt.Tests.Add(tdb);
             cxt.SaveChanges();
-            return View(tdb);
+            return View();
         }
+
 
     }
 }
